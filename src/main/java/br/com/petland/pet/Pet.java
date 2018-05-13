bed3d7a1d8b4abd0bcc5347b21baca312b3bc8a7
Moreira da  Silva <l.moreira.da.silva@accenture.com>
2018-05-13 19:13:16 -0300

    add pet controller get pet feature

---
+++ src/main/java/br/com/petland/pet/Pet.java
@@ -0,0 +1,5 @@
+package br.com.petland.pet;
+
+public class Pet {
+
+}
---
+++ src/main/java/br/com/petland/pet/PetController.java
@@ -0,0 +1,17 @@
+package br.com.petland.pet;
+
+public class PetController {
+	
+	PetService petService;
+	
+	public PetController(PetService petService) {
+		// TODO Auto-generated constructor stub
+		this.petService =  petService;
+	}
+
+	public Pet getPet(int id) {
+		// TODO Auto-generated method stub
+		return this.petService.getPet(id);
+	}
+
+}
